48d19a10ee7e2f510546d6eb258b15e6a6fd1984
galactic_ <102072394+MushyToast@users.noreply.github.com>
2022-12-21 17:51:39 +0000

    stuff

--- ai.py
+++ ai.py
@@ -1,10 +1,15 @@
-import openai
 import webbrowser
-key = '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'
+import openai
+import getkey
+
+key = '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'
 openai.api_key = key
 
 while True:
+    print("Use last prompt? Y for yes, N for no.")
+    key = getkey.getkey()
     ai_prompt = input("Enter in an image prompt: ")
+    lastinput = ai_prompt
     print('Generating..')
     response = openai.Image.create(
         prompt=ai_prompt,
@@ -14,7 +19,4 @@ while True:
     image_url = response['data'][0]['url']
     webbrowser.open_new_tab(image_url)
     print("Successfully generated!")
-
-
-
 
