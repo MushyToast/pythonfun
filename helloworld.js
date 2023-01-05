86b42278d268739207148201e1deffc8398f242b
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-01-05 17:47:59 +0000

    Jan 5, 2023, 5:47 PM

--- helloworld.js
+++ helloworld.js
@@ -1,10 +1,4 @@
 let names = ['Alice', 'Bob', 'Carol', 'Dave', 'Quandale Dingle'];
 
-function wait(time) {
-    function empty() {}
-    setTimeout(empty, time)
-}
-
-
 wait(100000);
 console.log(names[0]);
