f9c3096efe2608c211315a1281e1d2e47b5bf809
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-01-06 21:12:48 +0000

    Jan 6, 2023, 9:12 PM

--- nodjs/testing.js
+++ nodjs/testing.js
@@ -5,7 +5,10 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+var username
+
 rl.question('What is your name? ', (name) => {
   console.log(`Hello, ${name}`);
+  
   rl.close();
 });
