cde19765fac7b0b505bcdd2acf9be92f4894490a
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-01-06 16:12:04 +0000

    Jan 6, 2023, 4:12 PM

---
+++ htmlkaka.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const url = 'https://mushytoast.github.io/';
+
+http.get(url, (response) => {
+  let data = '';
+
+  response.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  response.on('end', () => {
+    console.log(data);
+  });
+}).on('error', (error) => {
+  console.error(error);
+});
