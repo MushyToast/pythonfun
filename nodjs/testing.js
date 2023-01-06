56f6219dcac2784330708cc5d5fcdfeab070791b
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-01-06 21:24:58 +0000

    Jan 6, 2023, 9:24 PM

--- nodjs/testing.js
+++ nodjs/testing.js
@@ -1,16 +1,23 @@
 const readline = require('readline');
 
+let name;
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-let username = '';
-
-rl.question('What is your name? ', (name) => {
-  console.log(`Hello, ${name}`);
-  username = name;
+rl.question('What is your name? ', (input) => {
+  name = input;
+  rl.close();
 });
 
-console.log(username)
-rl.close();
+rl.on('close', () => {
+  console.log(`Hello, ${name}`);
+});
+
+function printName(nameToPrint) {
+    console.log(nameToPrint);
+}
+
+setInterval(printName(name), 10)
