5ae059a54323217e76bb0f75ed29740efe00f38f
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-01-09 20:26:16 +0000

    Jan 9, 2023, 8:26 PM

---
+++ nodjs/kdown.js
@@ -0,0 +1,21 @@
+var stdin = process.stdin;
+
+// without this, we would only get streams once enter is pressed
+stdin.setRawMode( true );
+
+// resume stdin in the parent process (node app won't quit all by itself
+// unless an error or process.exit() happens)
+stdin.resume();
+
+// i don't want binary, do you?
+stdin.setEncoding( 'utf8' );
+
+// on any data into stdin
+stdin.on( 'data', function( key ){
+  // ctrl-c ( end of text )
+  if ( key === 'a' ) {
+    console.log("Hello!")
+  }
+  // write the key to stdout all normal like
+  process.stdout.write( key );
+});
