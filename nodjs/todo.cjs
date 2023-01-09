be4124d8a97b75670a9c857b43eccc031fd767ef
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-01-09 18:17:14 +0000

    Jan 9, 2023, 6:17 PM

--- nodjs/todo.cjs
+++ nodjs/todo.cjs
@@ -1,2 +1,11 @@
 const fs = require('fs');
+const readline = require('readline');
 
+const intf = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function typewriteText() {
+    
+}
