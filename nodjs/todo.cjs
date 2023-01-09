242e21829f4ed2a8a49755583db2d67fed35203e
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-01-09 20:36:12 +0000

    Jan 9, 2023, 8:36 PM

--- nodjs/todo.cjs
+++ nodjs/todo.cjs
@@ -6,8 +6,27 @@ const intf = readline.createInterface({
     output: process.stdout
 });
 
-function getkey() {
-  
+async function getkey() {
+  var stdin = process.stdin;
+
+// without this, we would only get streams once enter is pressed
+  stdin.setRawMode( true );
+
+// resume stdin in the parent process (node app won't quit all by itself
+// unless an error or process.exit() happens)
+  stdin.resume();
+
+// i don't want binary, do you?
+  stdin.setEncoding( 'utf8' );
+
+// on any data into stdin
+  stdin.on( 'data', function( key ){
+  // ctrl-c ( end of text )
+  if ( key === '\u0003' ) {
+    process.exit();
+  }
+  return key
+});
 }
 
 function type(text) {
