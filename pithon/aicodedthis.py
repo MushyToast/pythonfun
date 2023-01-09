547d8e135d2b9d630bc111d4ea800f208815bc8f
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-01-09 18:05:07 +0000

    Jan 9, 2023, 6:05 PM

--- pithon/aicodedthis.py
+++ pithon/aicodedthis.py
@@ -1,2 +1,15 @@
-name = input("What is your name? ")
-print("Hello, " + name)
+import time
+
+def typing_test(sentence):
+    start_time = time.time()
+    user_input = input(f"Type the following sentence:\n{sentence}\n")
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    if user_input == sentence:
+        print("Correct!")
+    else:
+        print("Incorrect!")
+    wpm = len(sentence.split()) / (elapsed_time / 60)
+    print(f"You typed at a speed of {wpm:.2f} words per minute.")
+
+typing_test("The quick brown fox jumps over the lazy dog")
