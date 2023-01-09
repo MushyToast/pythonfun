f372d0255813a9f2cb1ddf1c8728af2ae45208a1
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-01-09 21:25:58 +0000

    Jan 9, 2023, 9:25 PM

---
+++ pithon/encrypt.py
@@ -0,0 +1,37 @@
+wordflip = {
+    'a': 'z',
+    'b': 'y',
+    'c': 'x',
+    'd': 'w',
+    'e': 'v',
+    'f': 'u',
+    'g': 't',
+    'h': 's',
+    'i': 'r',
+    'j': 'q',
+    'k': 'p',
+    'l': 'o',
+    'm': 'n',
+    'n': 'm',
+    'o': 'l',
+    'p': 'k',
+    'q': 'j',
+    'r': 'i',
+    's': 'h',
+    't': 'g',
+    'u': 'f',
+    'v': 'e',
+    'w': 'd',
+    'x': 'c',
+    'y': 'b',
+    'z': 'a'
+}
+
+text = input("Enter in text to be encrypted\n")
+
+newtext = ""
+
+for char in text:
+    newtext += wordflip[char]
+
+print(f"Encrypted text: {newtext}")
