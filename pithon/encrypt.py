3955b27bb7516ca26a3104f236029aa20a0ad2c0
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-01-11 18:18:07 +0000

    Jan 11, 2023, 6:18 PM

--- pithon/encrypt.py
+++ pithon/encrypt.py
@@ -1,77 +1,11 @@
-import json
+from Crypto.Cipher import AES
 
-wordflip = {
-    'a': 'z',
-    'b': 'y',
-    'c': 'x',
-    'd': 'w',
-    'e': 'v',
-    'f': 'u',
-    'g': 't',
-    'h': 's',
-    'i': 'r',
-    'j': 'q',
-    'k': 'p',
-    'l': 'o',
-    'm': 'n',
-    'n': 'm',
-    'o': 'l',
-    'p': 'k',
-    'q': 'j',
-    'r': 'i',
-    's': 'h',
-    't': 'g',
-    'u': 'f',
-    'v': 'e',
-    'w': 'd',
-    'x': 'c',
-    'y': 'b',
-    'z': 'a'
-}
+message = input("Enter your message: ")
+key = input("Enter your key: ")
 
-wordum = {
-    'a': 1,
-    'b': 2,
-    'c': 3,
-    'd': 4,
-    'e': 5,
-    'f': 6,
-    'g': 7,
-    'h': 8,
-    'i': 9,
-    'j': 10,
-    'k': 11,
-    'l': 12,
-    'm': 13,
-    'n': 14,
-    'o': 15,
-    'p': 16,
-    'q': 17,
-    'r': 18,
-    's': 19,
-    't': 20,
-    'u': 21,
-    'v': 22,
-    'w': 23,
-    'x': 24,
-    'y': 25,
-    'z': 26
-}
+obj = AES.new(key)
+text = obj.encrypt(message)
 
-
-text = input("Enter in text to be encrypted\n")
-
-newtext = ""
-nText = 0
-
-for char in text:
-    if char.isalpha() == True:
-        newtext += wordflip[char.lower()]
-        nText += wordum[char.lower()]
-    else:
-        newtext += char
-newtext = nText
-print(f"Encrypted text: {newtext}")
-with open("encrypted.txt", "w") as f:
-    f.write(str(newtext))
-print("Saved to encrypted.txt")
+print(text)
+with open ('encrypted.txt', 'w') as f:
+    f.write(str(text))
