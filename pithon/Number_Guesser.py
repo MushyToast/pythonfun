ebae6a30d0a5c19ba17632fa8f7bff44449a67c3
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-01-13 15:16:33 +0000

    Jan 13, 2023, 3:16 PM

--- pithon/Number_Guesser.py
+++ pithon/Number_Guesser.py
@@ -1,29 +1,27 @@
 import random
 import time
+from getkey import getkey, keys
 
-rnumber = random.randint(1,999)
-gn = 0
-v2 = True
-
-while v2 == True:
-    def ask():
-        global gn
-        v1 = input("Enter Number Guess > ")
-        gn = int(v1)
+maxnumber = 999
 
 
-    ask()
-    print(gn)
-
-    def compare():
-        if gn > rnumber:
-            print("Number too high")
-        elif gn < rnumber:
-                print("Number too low")
-        elif gn == rnumber:
-            print("You got it!")
-            print("Program stopping in 3 seconds")
-            time.sleep(3)
-            quit()
-    compare()
-
+while True:
+    rnumber = random.randint(1,maxnumber)
+    guess = 0
+    tries = 0
+    
+    while True:
+        guess = int(input("Guess a number between 1 and %s: " % maxnumber))
+        tries += 1
+        if guess == rnumber:
+            print("You guessed it in %s tries!" % tries)
+            print("Play again? (y/n)")
+            key = getkey()
+            if key == "y" or key == "Y":
+                break
+            else:
+                exit()
+        elif guess > rnumber:
+            print("Too high!")
+        else:
+            print("Too low!")
