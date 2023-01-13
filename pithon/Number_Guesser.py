08a7bf73e8e03e825f14ed3996d8225383714805
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-01-13 15:23:17 +0000

    Jan 13, 2023, 3:23 PM

--- pithon/Number_Guesser.py
+++ pithon/Number_Guesser.py
@@ -19,7 +19,7 @@ while True:
         guess = int(input("Guess a number between 1 and %s: " % maxnumber))
         tries += 1
         if guess == rnumber:
-            print("You guessed it in %s tries!" % tries)
+            print(f"You guessed it in {tries} tries!")
             print("Play again? (y/n)")
             key = getkey()
             if key == "y" or key == "Y":
