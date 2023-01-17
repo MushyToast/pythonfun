c0299dccf199b8de2b4977594f0d4cf7372fd955
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-01-17 18:02:40 +0000

    Jan 17, 2023, 6:02 PM

--- nodjs/kdown.js
+++ nodjs/kdown.js
@@ -1,14 +1,19 @@
 const readLine = require('readline');
 const fs = require('fs');
-
-const rl = readLine.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
 var age = 0
 var name = ""
-rl.question("How old are you?\n", (input) =>{
-    age = input
-    console.log("You are " + input + " years old!")
-    rl.close();
-})
+function getName() {
+    const rl = readLine.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.question("What is your name? ", function (answer) {
+        name = answer
+        rl.close()
+    });
+    return name
+}
+
+name = getName()
+
+console.log("Welcome,", name)
