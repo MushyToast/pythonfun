9290b1d6ea530f9fe087df6f3241370eae5ff3dd
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-01-17 17:43:29 +0000

    Jan 17, 2023, 5:43 PM

--- nodjs/jswithtypes.js
+++ nodjs/jswithtypes.js
@@ -1,6 +1,9 @@
 var booleanValue = true;
+var counter = 0;
 while (booleanValue) {
     console.log("Hello World");
+    counter++;
+    console.log(counter);
     var determiner = Math.random();
     if (determiner == 1) {
         booleanValue = false;
--- nodjs/jswithtypes.ts
+++ nodjs/jswithtypes.ts
@@ -1,7 +1,9 @@
 var booleanValue: boolean = true;
-
+var counter: number = 0;
 while (booleanValue){
     console.log("Hello World");
+    counter++;
+    console.log(counter);
     let determiner = Math.random();
     if (determiner == 1){
         booleanValue = false;
