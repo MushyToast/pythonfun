59372d097568edf3834d78462fe831caaade2b48
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-01-17 17:32:01 +0000

    Jan 17, 2023, 5:32 PM

--- nodjs/helloworld.js
+++ nodjs/helloworld.js
@@ -2,7 +2,7 @@ var count = 0
 const randomwords = ['Hello', 'World', 'How', 'Are', 'You', 'Today', 'I', 'Am', 'Fine', 'Thank', 'You', 'Nice', 'To', 'Meet', 'You', 'Good', 'Bye', 'See', 'You', 'Tomorrow']
 function hw() {
   count += 1
-  console.log(randomwords[Math.floor(Math.random() * randomwords.length)] + ' ' + count)
+  console.log(randomwords)
 }
 
 setInterval(hw, 2)
