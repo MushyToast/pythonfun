7223ed930c5b7716d5fa2c719a2c85e4b49bdf62
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-01-18 20:44:03 +0000

    Jan 18, 2023, 8:44 PM

--- pithon/class.py
+++ pithon/class.py
@@ -1,5 +1,12 @@
 import random
-
+import asyncio
+count = 0
+async def checkNumber(number, desiredNumber, count):
+    if number == desiredNumber:
+        exit(f"Desired number found: {number}, after {count} tries")
 while True:
-    print(random.randint(1, 1000))
-    asy
+    count += 1
+    number = random.randint(1, 1000)
+    print(str(count) + ": " + number)
+    asyncio.run(checkNumber(number, 69, count))
+    
