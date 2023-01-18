66e9f24a973aa7c15c6c4946708cbbfe0da1fa1c
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-01-18 20:47:33 +0000

    Jan 18, 2023, 8:47 PM

--- pithon/class.py
+++ pithon/class.py
@@ -4,9 +4,12 @@ count = 0
 async def checkNumber(number, desiredNumber, count):
     if number == desiredNumber:
         exit(f"Desired number found: {number}, after {count} tries")
+min = input("Enter the minimum number: ")
+max = input("Enter the maximum number: ")
+desNum = input("Enter the desired number: ")
 while True:
     count += 1
-    number = random.randint(1, 1000)
-    print(str(count) + ": " + number)
-    asyncio.run(checkNumber(number, 69, count))
+    number = random.randint(1, 100000)
+    print(str(count) + ": " + str(number))
+    asyncio.run(checkNumber(number, 69420, count))
     
