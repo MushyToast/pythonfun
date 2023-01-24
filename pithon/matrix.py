8130bf1455a4f45bcb865cdcd84d7219d7f69e82
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-01-24 16:15:01 +0000

    Jan 24, 2023, 4:15 PM

--- pithon/matrix.py
+++ pithon/matrix.py
@@ -3,8 +3,31 @@ import time
 import random
 import pyquery
 import os
+import sys
+import pyfiglet
 #FUNCTIONS
 def clearscreen():
     os.system('clear')
 
-def scrolltext(text):
+def scrollText(text, delay=0.1):
+    for i in text:
+        print(i, end='')
+        sys.stdout.flush()
+        time.sleep(delay)
+
+def wait(secs):
+    time.sleep(secs)
+
+
+#PYFIGLET
+scrollText(pyfiglet.figlet_format("The", font="big"), 0.001)
+scrollText(pyfiglet.figlet_format("Matrix", font="big"), 0.001)
+
+#VARIABLES
+balance = 0
+job = "Unemployed"
+happiness = 100
+#ACTUAL CODE
+
+scrollText("This is the Matrix. A never ending cycle. Go to work. Get paid. Go to work. Get paid.")
+scrollText("You are fresh out of college. You have no job. You have no money. You are single.")
