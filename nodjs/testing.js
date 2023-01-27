4e2e925f814650213609700dbe1309f189c43905
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-01-27 21:18:35 +0000

    Jan 27, 2023, 9:18 PM

--- nodjs/testing.js
+++ nodjs/testing.js
@@ -1,4 +1,4 @@
-const https = require('https')
+const https = require('https');
 
 function getWpData(url) {
   return new Promise((resolve, reject) => {
@@ -7,6 +7,21 @@ function getWpData(url) {
       port: 443,
       path: '/index.html',
     }
-    
+    https.get(options, function(res){
+      res.on('data', function(d) {
+        return d
+        resolve(d);
+      });
+      res.on('error', function(e){
+        return e
+        reject(e);
+      });
+    });
   });
 }
+
+req = getWpData('www.google.com');
+
+req.on('resolve', function(data){
+  console.log(data);
+});
