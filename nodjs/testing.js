9705c581ecc4b083e0326302c14040b12b0fe31e
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-01-27 18:20:49 +0000

    a

--- nodjs/testing.js
+++ nodjs/testing.js
@@ -1,1 +1,15 @@
-const request = require('XMLHttpsRequest');
+const request = require('XMLHttpRequest').XMLHttpRequest;
+
+function getWpData(url) {
+  return new Promise((resolve, reject) => {
+    var req = new XMLHttpRequest
+    req.addEventListener("load", reqListener);
+    req.open("GET", "http://www.example.org/example.txt");
+    req.send();
+    return req
+    resolve()
+  });
+}
+
+
+
