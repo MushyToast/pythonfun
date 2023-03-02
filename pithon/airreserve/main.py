5cfdf6c3b254351b9390b6a2d35a6444e52e0b1d
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-03-02 15:51:41 +0000

    Mar 2, 2023, 3:51 PM

--- pithon/airreserve/main.py
+++
@@ -1,9 +0,0 @@
-import os
-import json
-
-plane1data = {}
-
-with open ('/home/codespaces/.python/current/bin/python3/workspaces/pythonfun/pithon/airreserve/plane1.json/' ,'r') as f:
-    plane1data = json.load(f)
-
-print(plane1data)
