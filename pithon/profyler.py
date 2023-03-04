565e8625c04221d6842d9e93743b6d8ace14b7ef
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-03-04 02:59:11 +0000

    Mar 4, 2023, 2:59 AM

---
+++ pithon/profyler.py
@@ -0,0 +1,31 @@
+import requests
+from time import sleep
+import random
+
+letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+numbers = ["1","2","3","4","5","6","7","8","9","0"]
+
+url = "https://api.profyle.net/api/users"
+
+params = {"userName":"quandale","displayName":"quandale","email":"h3gas7g@gmail.com","password":"qwu3ygg3gh4f73wg4897a"}
+
+while True:
+    passw = ""
+    for i in range(0, 100):
+        passw += random.choice(letters)
+        params["password"] = passw
+    name = ""
+    for i in range(0, 5):
+        name += random.choice(letters)
+        name += random.choice(numbers)
+        params["userName"] = name
+        params["displayName"] = name
+    email = ""
+    for i in range(0, 5):
+        email += random.choice(letters)
+        email += random.choice(numbers)
+        params["email"] = email + "@gmail.com"
+    print(params)
+    response = requests.request("POST", url, json=params)
+    print("\n\n\n\n")
+    print(response)
