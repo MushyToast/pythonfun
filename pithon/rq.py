cb12a52e6f3257108dd583bb7fd23a1f8f74155c
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-03-07 16:18:34 +0000

    Mar 7, 2023, 4:18 PM

--- pithon/rq.py
+++ pithon/rq.py
@@ -1,6 +1,10 @@
 import requests
 
+i = -1
 url = 'http://mushytoast.tech'
 while True:
-    r = requests.request("GET", url)
+    
+    r = requests.request("POST", url)
     print(r.status_code)
+    i += 1
+    print(str(i) + "\n\n\n\n")
