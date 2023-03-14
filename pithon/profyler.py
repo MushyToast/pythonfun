b687f0215056cb76743fbeb76ec326108481fb24
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-03-14 19:47:42 +0000

    Mar 14, 2023, 7:47 PM

--- pithon/profyler.py
+++ pithon/profyler.py
@@ -2,14 +2,17 @@ import requests
 from time import sleep
 import random
 import asyncio
+import os
 
 letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 numbers = ["1","2","3","4","5","6","7","8","9","0"]
 
 url = "https://api.profyle.net/api/users"
 
+os.seten
+
 params = {"userName":"quandale","displayName":"quandale","email":"h3gas7g@gmail.com","password":"qwu3ygg3gh4f73wg4897a"}
-it = 0
+
 while True:
     async def main():
         passw = ""
