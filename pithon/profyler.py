4fefda3d1cc13f347cfd41ebbef862feab1ca965
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-03-14 20:00:52 +0000

    Mar 14, 2023, 8:00 PM

--- pithon/profyler.py
+++ pithon/profyler.py
@@ -3,13 +3,16 @@ from time import sleep
 import random
 import asyncio
 import os
+import json
 
 letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 numbers = ["1","2","3","4","5","6","7","8","9","0"]
 
 url = "https://api.profyle.net/api/users"
-
-os.environ['iterator'] = '1'
+def getFileDir(dir):
+    here = os.path.dirname(os.path.abspath(__file__))
+    directory = os.path.join(here, dir)
+    return directory
 
 params = {"userName":"quandale","displayName":"quandale","email":"h3gas7g@gmail.com","password":"qwu3ygg3gh4f73wg4897a"}
 
@@ -30,8 +33,14 @@ while True:
             email += random.choice(letters)
             email += random.choice(numbers)
             params["email"] = email + "@gmail.com"
-        os.environ['iterator'] = str(int(os.environ['iterator']) + 1)
-        print(os.environ['iterator'] + ":")
+        dir = getFileDir("count.json")
+        with open(dir, "r") as f:
+            count = json.load(f)
+        count["count"] += 1
+        
+        with open(dir, "w") as f:
+            json.dump(count, f)
+        print(count["count"])
         print(params)
         response = requests.request("POST", url, json=params)
         print("\n\n\n\n")
