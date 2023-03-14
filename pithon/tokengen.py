27ce2ca6577c86d968f508c6f8b28e990ec3c204
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-03-14 20:37:24 +0000

    Mar 14, 2023, 8:37 PM

--- pithon/tokengen.py
+++ pithon/tokengen.py
@@ -4,10 +4,14 @@ letters = 'abcdefghijklmnopqrstuvwxyz'
 digits = '0123456789'
 
 def generate_token(length):
-    token = ''
+    token = 'OTG:-'
     for i in range(length):
         if random.randint(0, 1):
             token += random.choice(letters)
         else:
             token += random.choice(digits)
     return token
+
+print("\n\n\n\n\n")
+print("Token:")
+print(generate_token(560))
