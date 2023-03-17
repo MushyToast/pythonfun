adffde28aa5b36fd09b36b6fb0385693e65ccc6d
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-03-17 16:39:50 +0000

    Mar 17, 2023, 4:39 PM

--- pithon/chat.py
+++ pithon/chat.py
@@ -1,37 +1,3 @@
 import json
-import sys
 
-def deleteline(lines):
-    for x in range(1, lines):
-        sys.stdout.write("\033[F")
-        sys.stdout.write("\033[K")
-name = input("What is your name? ")
-
-def readchat():
-    with open('chatlog.json', 'r') as f:
-        data = json.load(f)
-    for key, value in data.items():
-        print(key + ": " + value)
-
-
-
-
-def writechat(new_data):
-    # Open the file in read mode
-    with open('chatlog.json', 'r') as f:
-        # Load the contents of the file into a list of dictionaries
-        data = json.load(f)
-
-    # Append the new data as a dictionary to the list
-    data.append(new_data)
-
-    # Open the file in write mode
-    with open('chatlog.json', 'w') as f:
-        # Write the updated data to the file
-        json.dump(data, f)
-
-while True:
-    deleteline(50)
-    readchat()
-    msg = input()
-    writechat({name: msg})
+data = {}
