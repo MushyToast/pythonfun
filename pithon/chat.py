8b880821c2ef9df91bab20709f0f3bce77fce0ba
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-03-17 16:42:52 +0000

    Mar 17, 2023, 4:42 PM

--- pithon/chat.py
+++ pithon/chat.py
@@ -1,3 +1,19 @@
 import json
+import os
 
 data = []
+
+def getFileDir(dir):
+    here = os.path.dirname(os.path.abspath(__file__))
+    directory = os.path.join(here, dir)
+    return directory
+
+filedir = getFileDir("chatlog.json")
+
+with open(filedir, "r") as f:
+    data = json.load(f)
+
+def saveChats():
+    with open(filedir, "w") as f:
+        json.dump(data, f)
+
