8f79af85398085bea27a831cc9c9e792eea68329
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-03-22 14:46:01 +0000

    Mar 22, 2023, 2:46 PM

--- pithon/tipe/tipe.py
+++ pithon/tipe/tipe.py
@@ -1,12 +1,12 @@
 import pyquery as pi
 import random
 import json
+from colorama import Fore
 
-words = []
+words = ""
 
 with open('pithon/tipe/words.json', 'r') as f:
     total = json.load(f)
     for x in range(1, 10):
-        words.append(random.choice(total))
-for x in words:
-    print(x)
+        words += (random.choice(total)) + " "
+print(words)
