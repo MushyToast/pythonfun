fe62864b5dd965b5befe4c7c69c784d2e8e43fb8
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-03-27 15:00:23 +0000

    Mar 27, 2023, 3:00 PM

--- pithon/pnoise.py
+++ pithon/pnoise.py
@@ -4,22 +4,23 @@ import sys
 
 noise = PerlinNoise(octaves=20, seed=random.randint(0, 999999999999))
 
-resx = 10
-resy = 10
-scale = 10000
+resx = 15
+resy = 15
+scale = 1000
 
 def get_symbol(noisevalue):
-    if noisevalue < 0.002:
+    if noisevalue > 0.02:
         return 'a'
-    elif noisevalue < 0.004:
+    elif noisevalue > 0.04:
         return 'b'
-    elif noisevalue < 0.006:
+    elif noisevalue > 0.07:
         return 'c'
-    elif noisevalue < 0.008:
+    elif noisevalue > 0.09:
         return 'd'
     else:
         return 'e'
 
 for x in range(resx):
     for y in range(resy):
-        print(noise([x/scale, y/scale]))
+        sys.stdout.write(get_symbol(noise([x/scale, y/scale])))
+    print('')
