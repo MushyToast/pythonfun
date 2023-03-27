571a981f28d2e5172aa54a3aef8a6914b2d3f8d1
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-03-27 16:56:41 +0000

    Mar 27, 2023, 4:56 PM

--- pithon/pnoise.py
+++ pithon/pnoise.py
@@ -2,18 +2,6 @@ from perlin_noise import PerlinNoise
 import random
 import sys
 
-customseed = 730961673295
-seed = 0
-if customseed == 0:
-    seed = random.randint(0, 999999999999)
-else:
-    seed = customseed
-
-noise = PerlinNoise(octaves=2, seed=seed)
-
-resx = 20
-resy = 20
-scale = 50
 
 def get_symbol(noisevalue):
     if noisevalue < -0.2:
@@ -27,14 +15,33 @@ def get_symbol(noisevalue):
     else:
         return '🟨'
 
-cells = {}
+def render(resx, resy, scale, seed, octaves):
+    if seed == 0:
+        seed = random.randint(0, 999999999999)
+    noise = PerlinNoise(octaves=octaves, seed=seed)
+    for x in range(resx):
+        for y in range(resy):
+            symbol = get_symbol(noise([x/scale, y/scale]))
+            sys.stdout.write(symbol)
+        print('')
 
+customseed = 730961673295
+seed = 0
+if customseed == 0:
+    seed = random.randint(0, 999999999999)
+else:
+    seed = customseed
+
+noise = PerlinNoise(octaves=2, seed=seed)
+
+resx = 20
+resy = 20
+scale = 50
 
 for x in range(resx):
     for y in range(resy):
         symbol = get_symbol(noise([x/scale, y/scale]))
         sys.stdout.write(symbol)
-        cells[str(x) + ", " + str(y)] = symbol
     print('')
 
 print("seed: " + str(noise.seed))
