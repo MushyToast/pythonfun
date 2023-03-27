d9d2300ee1e1df980daa8f231eb549f22018784d
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-03-27 16:39:03 +0000

    Mar 27, 2023, 4:39 PM

--- pithon/pnoise.py
+++ pithon/pnoise.py
@@ -24,9 +24,9 @@ cells = {}
 
 for x in range(resx):
     sys.stdout.write(str(x))
+for x in range(resx):
+    sys.stdout.write(str(x))
     for y in range(resy):
-        if y == 0:
-            sys.stdout.write(str(y))
         symbol = get_symbol(noise([x/scale, y/scale]))
         sys.stdout.write(symbol)
         cells[str(x) + ", " + str(y)] = symbol
