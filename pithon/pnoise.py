c9fdd7d916d0967c5e30b90f5973a1a6b963a5c5
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-03-28 20:38:28 +0000

    Mar 28, 2023, 8:38 PM

--- pithon/pnoise.py
+++ pithon/pnoise.py
@@ -25,8 +25,8 @@ def get_symbol(noisevalue):
     else:
         return '🟦'
 itemCells = {}
-for x in range(900):
-    itemCells[str(random.randint(-100, 100)) + ', ' + str(random.randint(-100, 100))] = '🪙'
+for x in range(2000):
+    itemCells[str(random.randint(-1000, 1000)) + ', ' + str(random.randint(-1000, 1000))] = '🪙'
 def getcell(x, y, seed, octaves, scale):
     noise = PerlinNoise(octaves=octaves, seed=seed)
     symbol = get_symbol(noise([x/scale, y/scale]))
