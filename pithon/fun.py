6ab59efbec3bf0b207df250822d92ac0cd499519
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-03-28 20:01:34 +0000

    Mar 28, 2023, 8:01 PM

--- pithon/fun.py
+++ pithon/fun.py
@@ -1,16 +1,5 @@
-from datetime import datetime
+stuff = {
+    '0, 0': '🧍'
+}
 
-def gettime(): 
-    time = ""
-    time += str(datetime.now().year)
-    time += "-"
-    time += str(datetime.now().month)
-    time += "-"
-    time += str(datetime.now().day)
-    time += " "
-    time += str(datetime.now().hour)
-    time += ":"
-    time += str(datetime.now().minute)
-    print(time)
-
-gettime()
+print(stuff['3, 5'])
