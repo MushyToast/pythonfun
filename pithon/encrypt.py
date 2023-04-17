ab321df71134c672aee88b74dd98329b2b0e5011
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-04-17 17:13:53 +0000

    Apr 17, 2023, 5:13 PM

--- pithon/encrypt.py
+++ pithon/encrypt.py
@@ -2,4 +2,4 @@ from Crypto.Cipher import AES
 obj = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
 message = "The answer is no"
 ciphertext = obj.encrypt(message)
-ciphertext
+print(ciphertext)
