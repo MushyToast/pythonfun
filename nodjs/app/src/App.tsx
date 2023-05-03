8be452635a8c7dca77d8076c09947d5c801a7f84
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-05-03 19:36:49 +0000

    May 3, 2023, 7:36 PM

--- nodjs/app/src/App.tsx
+++ nodjs/app/src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import { useState } from 'react';
 
-const loggedIn = false;
-
 function App() {
   const [likes, setLikes] = useState(0);
+  const [btnText, setBtnText] = useState("Like");
   const [liked, setLike] = useState(0);
   function toggleLike() {
     if (liked === 1) {
       setLike(0);
       setLikes(likes - 1)
+      setBtnText("Like")
     } else {
       setLike(1);
       setLikes(likes + 1)
+      setBtnText("Unlike")
     } 
   }
   return (
     <div className="App">
       <button onClick={toggleLike}>
-        Like
+        {btnText}
       </button>
       <p>{likes}</p>
     </div>
