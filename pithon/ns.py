8b116b37ce0bf955fe48a77f37104f30442c26ff
galactic_ <102072394+MushyToast@users.noreply.github.com>
2023-05-23 17:38:18 +0000

    May 23, 2023, 5:38 PM

--- pithon/ns.py
+++ pithon/ns.py
@@ -6,6 +6,15 @@ noise = PerlinNoise(octaves=10, seed=random.randint(-999999999999, 999999999999)
 
 def get_symbol(noisevalue):
     if noisevalue < -0.1:
-        return Style.DIM + "●"
-    elif noisevalue < -0.2:
-        return Fore.BLACK + "●"
+        return Style.DIM + "●" + Style.RESET_ALL
+    elif noisevalue < 0.1:
+        return Style.NORMAL + "●" + Style.RESET_ALL
+    else:
+        return Style.BRIGHT + "●" + Style.RESET_ALL
+    
+
+for x in range(0, 100):
+    for y in range(0, 100):
+        symbol = get_symbol(noise([x/100, y/100]))
+        print(symbol, end='')
+    print('')
